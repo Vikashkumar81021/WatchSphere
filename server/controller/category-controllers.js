2c09fca32914f8ba7bcb70fda00dbb21d4d1ffab
Vikash Kumar Singh <kumarrohit93334@gmail.com>
2024-09-02 22:43:35 +0530

    add crud operation of category

--- server/controller/category-controllers.js
+++ server/controller/category-controllers.js
@@ -28,6 +28,72 @@ export const createCategoryController = async (req, res) => {
   } catch (error) {
     res
       .status(500)
-      .json({ message: "Error creating category", error: error.message });
+      .json({ message: "Error while creating category", error: error.message });
+  }
+};
+
+export const updateCategoryController = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { id } = req.params;
+    const category = await categoryModel.findByIdAndUpdate(
+      id,
+      { name, slug: slugify(name) },
+      { new: true }
+    );
+    res.status(200).json({
+      success: true,
+      message: "category updated successfully",
+      category,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error while updating category", error: error.message });
+  }
+};
+
+export const getAllCategories = async (req, res) => {
+  try {
+    const category = await categoryModel.find({});
+    return res.status(200).json({
+      message: "All Categories List",
+      category,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error while getting category", error: error.message });
+  }
+};
+export const getAllSingleCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const category = await categoryModel.findById(id);
+    return res.status(200).json({
+      message: "Get single category Successfully",
+      category,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error while getting single  category",
+      error: error.message,
+    });
+  }
+};
+
+export const deleteAllCategories = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const category = await categoryModel.findByIdAndDelete(id);
+    return res.status(200).json({
+      message: "category deleted successfully",
+      success: true,
+      category,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error while delete categoryy",
+    });
   }
 };
--- server/routes/category-routes.js
+++ server/routes/category-routes.js
@@ -1,7 +1,27 @@
 import express from "express";
 import { isAdmin, ProtectedRoute } from "../middlewares/auth-middleware.js";
-import { createCategoryController } from "../controller/category-controllers.js";
+import {
+  createCategoryController,
+  updateCategoryController,
+  getAllCategories,
+  deleteAllCategories,
+  getAllSingleCategory,
+} from "../controller/category-controllers.js";
 const router = express.Router();
 router.post("/category", ProtectedRoute, isAdmin, createCategoryController);
-
+router.put(
+  "/update-category/:id",
+  ProtectedRoute,
+  isAdmin,
+  updateCategoryController
+);
+router.get("/allCategories", getAllCategories);
+router.delete(
+  "/deleteAllCategories/:id",
+  ProtectedRoute,
+  isAdmin,
+  deleteAllCategories
+);
+//single category
+router.get("/getSingleCategory/:id", getAllSingleCategory);
 export default router;
